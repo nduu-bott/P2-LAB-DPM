69f41c2eee5f5c141286e2258323292732d7c69a
nduu-bott <pandusarma03@gmail.com>
2024-12-03 23:13:31 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Teks di tengah layar */}
+      <Text style={styles.text}>PANDU SARMA</Text>
+
+      {/* Kotak-kotak dengan FlexBox */}
+      <View style={styles.boxContainer}>
+        <View style={[styles.box, { backgroundColor: "#FF6347" }]} />
+        <View style={[styles.box, { backgroundColor: "#4682B4" }]} />
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#D3D3D3",
+  },
+  text: {
+    fontSize: 24,
+    color: "blue",
+    fontWeight: "bold",
+    marginBottom: 50,
+  },
+  boxContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "80%",
+  },
+  box: {
+    width: 100,
+    height: 100,
   },
 });
+
+export default App;
